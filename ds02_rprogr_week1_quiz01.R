3d21487a2fcb1cd5f8cc9516e6b5bef15a1244f4
Greg1717 <gergely_horvath@icloud.com>
2020-12-17 09:45:55 +0100

    finalized week 1

--- ds02_rprogr_week1_quiz01.R
+++ ds02_rprogr_week1_quiz01.R
@@ -1,2 +1,15 @@
 
 # notes for Course 2 on R Programming, Week 1, quiz 1
+
+df <- read.csv(file = "hw1_data.csv")
+df
+df[1:2,]
+str(df)
+tail(df, 2)
+df$Ozone[[47]]
+df[47,]
+nrow(df[is.na(df$Ozone),])
+mean(df$Ozone, na.rm = TRUE)
+mean(df[df$Ozone > 31 & df$Temp > 90, 2], na.rm = TRUE)
+mean(df[df$Month == 6L, "Temp"])
+max(df[df$Month == 5L, 1], na.rm = TRUE)
